c0ba5116b896aca07d1f7ceb12b7df5c6fc93823
Nicole Xirakia <nxirakia@gmail.com>
2017-06-14 23:32:01 +0200

    Movie queries and movie dropdown options

--- src/js/MovieService.js
+++ src/js/MovieService.js
@@ -2,19 +2,7 @@ import { db } from 'baqend/realtime'
 
 class MovieService {
 
-    /**
-     * Loads movie suggestions for the typeahead input
-     * @param {string} [title] The movie title
-     */
-    loadMovieSuggestions(title) {
-        //TODO
-        let query = db.Movie.find()
-            .where({ 'id': { '$exists' : true } })
-			.sort({ 'id': -1 })
-            .limit(10);
 
-        return query.resultList((results) => results.map((result) => result.title));
-    }
 
     /**
      * Loads a specific movie by title
@@ -23,12 +11,83 @@ class MovieService {
     loadMovieByTitle(title) {
         //TODO
         let query = db.Movie.find()
-            .where({ 'id': { '$exists' : true } })
-			.sort({ 'id': -1 });
-
-        return query.singleResult();
+            .where({ 'title': title})
+			.sort({ 'id': -1 })
+            .limit(1);
+        return query;
     }
 
+    loadAllMovies(limit){
+       let query = db.Movie.find()
+            .where({ 'id': { '$exists' : true } })
+            .sort({ 'id': -1 })
+            .limit(new Number(limit));
+       return query;
+    }
+    /**
+     * Loads movie suggestions for the typeahead input
+     * @param {string} [title] The movie title
+     */
+    loadMoviesByPrefix(prefix, limit){
+        var pattern = "^" + prefix;
+        let query = db.Movie.find()
+            .where({'title': {$regex: pattern}})
+            .sort({'id': -1 })
+            .limit(new Number(limit));
+        return query;
+    }
+
+    loadMoviesByGenre(genre, limit){
+        let query = db.Movie.find()
+            .where({'genre': {'$in': genre.split(',')}})
+            .sort({'id': -1 })
+            .limit(new Number(limit));
+        return query;
+    }
+
+    loadMoviesByRatingGt(rating,limit){
+        let query = db.Movie.find()
+            .where({'rating': {$gt: parseInt(rating)}})
+            .sort({'id': -1 })
+            .limit(new Number(limit));
+        return query;
+    }
+
+    loadMoviesByGenrePartialMatch(genre, limit){
+        var pattern = "^" + genre;
+        let query = db.Movie.find()
+            .where({'genre': {$regex: pattern}})
+            .sort({'id': -1 })
+            .limit(new Number(limit));
+        return query;
+    }
+
+    loadMoviesByReleasedDateCountry(country, limit){
+        var isodate = new Date("1950-01-01T01:00:00Z").getTime();
+        console.log(isodate)
+        let query = db.Movie.find()
+            .where({'releases':{ $elemMatch: {'date': {$lte:{'$date': isodate}}, 'country': country}}})
+            .sort({'id': -1 })
+            .limit(new Number(limit));
+        console.log(query)
+        return query;
+    }
+
+    loadMoviesByUserComment(username, limit){
+        let comment= this.commentsByUser(username);
+        console.log(comment)
+        let query = db.Movie.find()
+            .where({'id': {$in: comment.toArray()}})
+            .sort({ 'id': -1 })
+            .limit(new Number(limit));
+        return query;
+    }
+
+    commentsByUser(username) {
+        let query = db.MovieComment.find()
+            .where({"username": username})
+        return query.toArray();
+    }
     /**
      * Queries movies filtered by the query arguments
      * @param {Object} [args] The query arguments
@@ -38,28 +97,32 @@ class MovieService {
      */
     queryMovies(args) {
         let query;
-
-        console.log("args type ", args.parameter)
-
         switch (args.type) {
-            case 'prefix':
-                query = db.Movie.find()
-                    .where({ 'id': { '$exists' : true } })
-                    .sort({ 'id': -1 })
-                    .limit(new Number(args.limit));
+            case 'show-all':
+                query = this.loadAllMovies(args.limit);
                 break;
-
             case 'genre':
-                    query = db.Movie.find()
-                        .where({'genre': args.parameter})
-                        .sort({'id': -1 })
-                        .limit(new Number(args.limit));
-
-
+                query = this.loadMoviesByGenre(args.parameter, args.limit);
                 break;
-
+            case 'prefix':
+                query= this.loadMoviesByPrefix(args.parameter, args.limit);
+                break;
+            case 'full-title':
+                query = this.loadMovieByTitle(args.parameter);
+                break;
+            case 'rating-greater':
+                query = this.loadMoviesByRatingGt(args.parameter, args.limit);
+                break;
+            case 'genrePartialmatch':
+                query = this.loadMoviesByGenrePartialMatch(args.parameter, args.limit);
+                break;
+           /* case 'release':
+                query = this.loadMoviesByReleasedDateCountry(args.parameter, args.limit);
+                break;
+            case 'comments':
+                query = this.loadMoviesByUserComment(args.parameter, args.limit);
+                break; */
         }
-
         return query.resultList();
     }
 
